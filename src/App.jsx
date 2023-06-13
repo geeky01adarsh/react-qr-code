15b13fddeec7b11d06211aba957424bc922199fc
geeky01adarsh <adarsh91094@gmail.com>
2023-06-13 18:13:56 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
 import QRcodeGenerator from "./components/QRcodeGenerator";
 import QRcodeScanner from "./components/QRcodeScanner";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hey there!!!</h1>
+        <p>What you want to do?</p>
+        <Link to="scan">Scan a QR code</Link> <br/>
+        <Link to="create">Create a QR code</Link>
+      </div>
+    ),
+  },
+  {
+    path: "scan",
+    element: <QRcodeScanner/>,
+  },{
+    path: "create",
+    element: <QRcodeGenerator/>,
+  },
+]);
 
 function App() {
   return (
     <>
-      <QRcodeGenerator/>
-      <QRcodeScanner/>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/components/QRcodeGenerator.jsx
+++ src/components/QRcodeGenerator.jsx
@@ -7,6 +7,26 @@ const QRcodeGenerator = () => {
   const [back, setBack] = useState("#FFFFFF");
   const [fore, setFore] = useState("#000000");
   const [size, setSize] = useState(256);
+
+  const onImageDownload = () => {
+    const svg = document.getElementById("QRCode");
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const pngFile = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.download = "QRCode";
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  };
+
   return (
     <div>
       <div className="App">
@@ -20,38 +40,20 @@ const QRcodeGenerator = () => {
           />
           <br />
           <br />
-          {/* <input
-            type="text"
-            onChange={(e) => setBack(e.target.value)}
-            placeholder="Background color"
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            onChange={(e) => setFore(e.target.value)}
-            placeholder="Foreground color"
-          />
-          <br />
-          <br /> */}
-          {/* <input
-            type="number"
-            onChange={(e) =>
-              setSize(parseInt(e.target.value === "" ? 0 : e.target.value, 10))
-            }
-            placeholder="Size of Qr-code"
-          />
-          <br />
-          <br /> */}
           <br />
           {value && (
-            <QRCode
-              title="Attendance"
-              value={value}
-              // bgColor={back}
-              // fgColor={fore}
-              size={size === "" ? 250 : size}
-            />
+            <>
+              <QRCode
+                title="Attendance"
+                value={value}
+                id="QRCode"
+                // bgColor={back}
+                // fgColor={fore}
+                size={size === "" ? 250 : size}
+              />
+              <br/>
+              <button onClick={onImageDownload}>Download</button>
+            </>
           )}
         </center>
       </div>
